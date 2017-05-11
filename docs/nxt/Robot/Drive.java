9601774fb7a39a88b3ccb58d6b74eeaa541ec082
cteckholm <CTEckholm@student.naperville203.org>
2017-05-11 14:13:30 -0500

    Both Wheelas

--- docs/nxt/Robot/Drive.java
+++ docs/nxt/Robot/Drive.java
@@ -1,21 +1,17 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
+import lejos.robotics.navigation.*;
 public class Drive{
     private static int highestLight;
     public static void main(String[] agrs){
-        NXTMotor LEFT = Mtor.LEFT;
-        NXTMotor RIGHT = Mtor.RIGHT;
-        
+        NXTRegulatedMotor LEFT = Mtor.LEFT;
+        NXTRegulatedMotor RIGHT = Mtor.RIGHT;
+        DifferentialPilot pilot = new DifferentialPilot(56.0, 100.0, LEFT, RIGHT, true);
         LightSensor LI = Sensors.LI;
-        Button.ENTER.waitForPressAndRelease();
-        while(true){ 
-            
-            
-            while(LI.getLightValue() > 5){
-                LEFT.forward();
-            }
-           
+        int rep = 0;
+        while(rep < 3){
+            pilot.forward();
         }
-        
+        pilot.stop();
     }
 }
--- docs/nxt/Robot/Mtor.java
+++ docs/nxt/Robot/Mtor.java
@@ -1,6 +1,6 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.*;
 public class Mtor{
-    public static NXTMotor LEFT = new NXTMotor(MotorPort.A);
-    public static NXTMotor RIGHT = new NXTMotor(MotorPort.B);
+    public static NXTRegulatedMotor LEFT = new NXTRegulatedMotor(MotorPort.A);
+    public static NXTRegulatedMotor RIGHT = new NXTRegulatedMotor(MotorPort.B);
 }
